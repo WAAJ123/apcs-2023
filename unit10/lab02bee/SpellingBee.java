4cc6cfcec752bebcd7605f3dd026ccbcf2314c4f
Aaron <127899108+WAAJ123@users.noreply.github.com>
2023-03-23 21:46:08 +0000

    started the Spelling bee work and followed along with the in class code

--- unit10/lab02bee/SpellingBee.java
+++ unit10/lab02bee/SpellingBee.java
@@ -8,12 +8,25 @@ public class SpellingBee {
 
     private char[] letters;
     private char mustUse;
-
-    // TODO construct me!
-
-    public boolean checkWord(String word) {
+     
+ /*   public boolean checkWord(String word) {
         // TODO implement me!
-        return true;
+        if (word.length() < 4 || word.indexOf(mustUse) == -1) {
+            return false;
+        }
+        
+        char[] characters = word.toCharArray();
+        String letWord = new String(letters);
+        
+        for (int f = 0; f <= characters.length(); f++) {
+            if (letWord.indexOf(f) == -1) {
+                return false;
+            }
+        } */
+
+    public SpellingBee(char[] letters, char mustUse) {
+        this.letters = letters;
+        this.mustUse = mustUse;
     }
 
     /**
@@ -40,11 +53,41 @@ public class SpellingBee {
         String[] words = loadFile("words_dropped.txt").split("\n");
         System.out.println("Loaded " + words.length + " words");
         // TODO solve me!
-        // SpellingBee bee = new SpellingBee("ranglty".toCharArray(), 'y');
+        SpellingBee bee = new SpellingBee("ranglty".toCharArray(), 'y');
 
         // TODO sort words!
 
         // TODO what position in the sorted list is the word "search" ?
+        Arrays.sort(words);
+        // linear search
+        int n = 0;
+        for (String word : words) {
+            n++;
+            if (word.equals(anObject:"potato")) {
+            //    System.out.println("found potato at " + n);
+            }
+        }
+        String findMe = "potato";
+
+        int begin = 0;
+        int end = words.length - 1;
+        int guess = (begin + end) / 2;
+        int check = word[guess].compareTo(findMe);
+        System.out.println("word is " + words[guess]);
+        System.out.println("check is " + check);
+        while  (check != 0) {
+            if (guess < 0) {
+                begin = guess;
+            }
+            else if (check > 0) [
+                end = guess;
+            ]
+            guess = (begin + end) / 2;
+            int check = word[guess].compareTo(findMe);
+            System.out.println("word is " + words[guess]);
+            System.out.println("check is " + check);
+        }
+        begin = guess;
 
     }
 }
