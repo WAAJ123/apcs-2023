e1306c6294fca55c4918a62a7c918e85d53fbcba
Aaron <127899108+WAAJ123@users.noreply.github.com>
2023-03-24 21:44:18 +0000

    finished checkWord method and solved the SpellingBee puzzle

--- unit10/lab02bee/SpellingBee.java
+++ unit10/lab02bee/SpellingBee.java
@@ -3,26 +3,29 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.zip.CheckedInputStream;
 
 public class SpellingBee {
 
     private char[] letters;
     private char mustUse;
      
- /*   public boolean checkWord(String word) {
-        // TODO implement me!
+    public boolean checkWord(String word) {
         if (word.length() < 4 || word.indexOf(mustUse) == -1) {
             return false;
         }
-        
+
         char[] characters = word.toCharArray();
         String letWord = new String(letters);
         
-        for (int f = 0; f <= characters.length(); f++) {
-            if (letWord.indexOf(f) == -1) {
+        for (char character : characters) {
+            if (letWord.indexOf(character) == -1) {
                 return false;
             }
-        } */
+        }
+        return true;
+    }
 
     public SpellingBee(char[] letters, char mustUse) {
         this.letters = letters;
@@ -52,42 +55,45 @@ public class SpellingBee {
     public static void main(String[] args) {
         String[] words = loadFile("words_dropped.txt").split("\n");
         System.out.println("Loaded " + words.length + " words");
-        // TODO solve me!
+
         SpellingBee bee = new SpellingBee("ranglty".toCharArray(), 'y');
-
-        // TODO sort words!
-
-        // TODO what position in the sorted list is the word "search" ?
-        Arrays.sort(words);
-        // linear search
-        int n = 0;
-        for (String word : words) {
-            n++;
-            if (word.equals(anObject:"potato")) {
-            //    System.out.println("found potato at " + n);
+        
+        for(String word : words) {
+            if (bee.checkWord(word)) {
+                System.out.println(word);
             }
         }
-        String findMe = "potato";
+  
+        // Arrays.sort(words);
+        // // linear search
+        // int n = 0;
+        // for (String word : words) {
+        //     n++;
+        //     if (word.equals("potato")) {
+        //     //    System.out.println("found potato at " + n);
+        //     }
+        // }
+        // String findMe = "potato";
 
-        int begin = 0;
-        int end = words.length - 1;
-        int guess = (begin + end) / 2;
-        int check = word[guess].compareTo(findMe);
-        System.out.println("word is " + words[guess]);
-        System.out.println("check is " + check);
-        while  (check != 0) {
-            if (guess < 0) {
-                begin = guess;
-            }
-            else if (check > 0) [
-                end = guess;
-            ]
-            guess = (begin + end) / 2;
-            int check = word[guess].compareTo(findMe);
-            System.out.println("word is " + words[guess]);
-            System.out.println("check is " + check);
-        }
-        begin = guess;
+        // int begin = 0;
+        // int end = words.length - 1;
+        // int guess = (begin + end) / 2;
+        // int check = words[guess].compareTo(findMe);
+        // System.out.println("word is " + words[guess]);
+        // System.out.println("check is " + check);
+        // while  (check != 0) {
+        //     if (guess < 0) {
+        //         begin = guess;
+        //     }
+        //     else if (check > 0) {
+        //         end = guess;
+        //     }
+        //     guess = (begin + end) / 2;
+        //     check = words[guess].compareTo(findMe);
+        //     System.out.println("word is " + words[guess]);
+        //     System.out.println("check is " + check);
+        // }
+        // begin = guess;
 
     }
 }
