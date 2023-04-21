7c7c59af1c11e4f5e201fbc92e22139810f7fd70
Aaron <127899108+WAAJ123@users.noreply.github.com>
2023-04-21 21:45:31 +0000

    we have pi!

--- unit10/PiDay.java
+++ unit10/PiDay.java
@@ -1,3 +1,4 @@
+package unit10;
 public class PiDay {
 
     // infinite series for π
@@ -5,21 +6,24 @@ public class PiDay {
     public static double piDay(int n, boolean subtract) {
       // TODO
       //base case to prevent stack overflow (though technically pi should have it)
-      if (n == 4/1001) {
-        return ;
+      if (n >= 10000) {
+        return (double)4/n;
       }
       else {
-        return  
+        if (subtract) {
+          return -4.0/n + piDay(n+2, false);
+        }
+          return 4.0/n + piDay(n+2, true);
       }
 
     }
 
     public static void main(String args[]) {
         //  Gregory–Leibniz
-        //double pi = piDay(1, false);
+        double pi = piDay(1, false);
 
         // Nilakantha
-        double pi = 3.0 + piDay(3, false);
+    //     double pi = 3.0 + piDay(3, false);
 
         System.out.println("pi = " + pi);
     }
