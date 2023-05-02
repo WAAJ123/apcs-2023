e073f98d72fff2bf08f79ddda8d94fb46291c3e6
Aaron <127899108+WAAJ123@users.noreply.github.com>
2023-05-02 21:19:58 +0000

    boom gettin stuff done

--- unit10/Recur.java
+++ unit10/Recur.java
@@ -14,10 +14,7 @@ public class Recur {
         }
     }
 
-    // f(3) = 2
-    // called f(3)
-    // return f(2) + f(1) == 2
-    // f(2) will return f(1) + f(0) == 1 
+    // f(3) = 2 
     public static int f(int n) {
         if (n == 0)
             return 0;
@@ -27,12 +24,16 @@ public class Recur {
             return f(n - 1) + f(n - 2);
     }
 
+    // hello returns true
+    // howdy returns false
     public static boolean has(String input) {
         if (input.length() < 2)
             return false;
         return (input.charAt(0) == input.charAt(1) || has(input.substring(1)));
     }
 
+    //deified returns true
+    //deepfried returns false
     public static boolean pot(String input) {
         if (input.length() < 2)
             return true;
@@ -40,29 +41,33 @@ public class Recur {
                 && pot(input.substring(1, input.length() - 1)));
     }
 
+    //two(6) returns a very precise decimal close to 0.4
     public static double two(int n) {
         if (n < 1)
             return 1.0 / 2;
         return 1.0 / (2 + two(n - 1));
     }
 
+    //mop(11) returns 1011
     public static String mop(int n) {
         if (n / 2 == 0)
             return "" + n % 2;
         return mop(n / 2) + n % 2;
     }
 
+    //cat(4096) returns 19
     public static int cat(int x) {
         if (x == 0)
             return x;
         return ((x % 10) + cat(x / 10));
     }
 
+    //fixed so that mystery(10)==32
     public static int mystery(int x) {
         if (x <= 1)
             return 1;
         // return mystery(x - 1) + mystery(x - 2);
-        // return 2 * mystery(x - 2);
+         return 2 * mystery(x - 2); //this is the right one
         // return 2 * mystery(x - 1);
         // return 4 * mystery(x - 4);
         return 4 + mystery(x - 1);
@@ -74,11 +79,11 @@ public class Recur {
         // System.out.println(fun3(5));
 
         // Consider function f
-        System.out.println(f(3));
+        //System.out.println(f(3));
 
         // Consider function has
-        // System.out.println(has("hello"));
-        // System.out.println(has("howdy"));
+        //System.out.println(has("hello"));
+        //System.out.println(has("howdy"));
 
         // Consider function pot
         // System.out.println(pot("deified"));
